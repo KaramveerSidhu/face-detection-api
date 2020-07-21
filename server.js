626742520205a66d2a5fda5eb60205c745e34258
KaramveerSidhu <karamveersidhu14@gmail.com>
2020-07-21 07:25:40 +0530

    Adding dynamic port

--- server.js
+++ server.js
@@ -33,6 +33,6 @@ app.get('/profile/:id', (req,res) => { profile.handleProfileGet(req, res, db) })
 app.put('/image', (req,res) => { image.handleImage(req, res, db) })
 app.post('/imageurl', (req, res) => { image.handleApiCall(req, res) })
 
-app.listen(3000, () => {
-	console.log('App is running!')
+app.listen(process.env.PORT || 3000, () => {
+	console.log(`App is running! on PORT ${process.env.PORT}`)
 })
